da961360a64faa66e983e52339b866d27620c773
Kumar Mani <ctpkmani@gmail.com>
2020-03-18 07:34:04 -0700

    anupythonfile

---
+++ anu1.py
@@ -0,0 +1,31 @@
+
+
+Class Employee:
+    Company_name="TCS"
+    Company_address="HYD"
+    def __init__(self,eno,ename,edept,edesg,esal):
+        self.employee_no=eno
+        self.employee_name=ename
+        self.employee_department=edept
+        self.employee_designation=edesg
+        self.employee_salary=esal
+        print("self",id(self))
+    def display(self):
+        print("COMPANY NAME=",Employee.Company_name)
+        print("COMPANY ADDRESS=",Employee.Company_address)
+        print("EMPLOYEE ID=",self.employee_no)
+        print("EMPLOYEE NAME=",self.employee_name)
+        print("EMPLOYEE DEPARTMENT=",self.employee_department)
+        print("EMPLOYEE DESIGNATION=",self.employee_department)
+        print("EMPLOYEE SALARY=",self.employee_salary)
+    e1=Employee(1001,"ravi","SE","IT",100000)
+    print("e1",id(e1))
+    print("*",*40)
+    e2=Employee(1002,"raju","SE","IT",200000)
+    print("e2",id(e2))
+       
+
+    
+    
+employee.py
+Displaying employee.py.
